6129f6a4b43e950cdf7fbbb38d3f6fefa2652182
AlessioCherici <alessio.cherici@hotmail.it>
2023-01-10 17:47:57 +0100

    Aggiungere i file di progetto.

---
+++ Prova1/Models/Pizza.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyPizzeriaModel.Models
+    {
+    [Table("Pizzas")]
+    public class Pizza
+        {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Prezzo { get; set; }
+
+        public Pizza (string nome, string prezzo)
+            {
+            this.Nome = nome;
+            this.Prezzo = prezzo;
+            }
+        }
+    }
---
+++ Prova1/PizzaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPizzeriaModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPizzeriaModel
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
